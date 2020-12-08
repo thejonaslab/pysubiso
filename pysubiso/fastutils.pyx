f0b47c118a574b89ed7c71d181dda7f75d9242fc
Eric Jonas <jonas@ericjonas.com>
2020-12-08 16:16:12 -0600

    passing tests for node-edge-type checking

--- pysubiso/fastutils.pyx
+++ pysubiso/fastutils.pyx
@@ -3,15 +3,16 @@ cimport numpy as np
 
 cimport cython
 
+
 @cython.boundscheck(False)  # Deactivate bounds checking
 @cython.wraparound(False)   # Deactivate negative indexing.
 cdef _gen_possible_next_edges(np.int32_t[:,:] adj,
-                              np.int32_t[:] colors,
+                              np.int32_t[:] edge_colors,
                               np.int32_t[:, :] out):
     cdef Py_ssize_t i, j
     cdef np.int32_t c = 0 
     cdef Py_ssize_t pos = 0
-    cdef Py_ssize_t color_n = colors.shape[0]
+    cdef Py_ssize_t color_n = edge_colors.shape[0]
 
     
     for i in range(adj.shape[0]):
@@ -20,16 +21,19 @@ cdef _gen_possible_next_edges(np.int32_t[:,:] adj,
                 for c in range(color_n):
                     out[pos, 0] = i
                     out[pos, 1] = j
-                    out[pos, 2] = colors[c]
+                    out[pos, 2] = edge_colors[c]
                     pos += 1
     return pos
 
 
-cpdef gen_possible_next_edges(np.int32_t[:,:] adj, np.int32_t[:] colors):
+cpdef gen_possible_next_edges(np.int32_t[:,:] adj, np.int32_t[:] edge_colors):
     """
     Given a (symmetric) input adjacency matrix adj and an
     array of possible colors, compute all possible edges that could be added
     to the matrix and return. 
+
+    Note colors must be > 0 and we don't generate candidates for
+    nodes that already have edges between them. 
 
 
     Input: adj: N x N _symmetric_ adjacency matrix (np.int32)
@@ -41,12 +45,102 @@ cpdef gen_possible_next_edges(np.int32_t[:,:] adj, np.int32_t[:] colors):
     upper-triangular part of the matrix. 
     """
 
-    cpdef max_N = (adj.shape[0] * adj.shape[1] //2 * len(colors))
+    cpdef max_N = (adj.shape[0] * adj.shape[1] //2 * len(edge_colors))
     
 
     cpdef out = np.empty((max_N, 3), dtype=np.int32)
 
-    num_out = _gen_possible_next_edges(adj, colors, out)
+    num_out = _gen_possible_next_edges(adj, edge_colors, out)
     out.resize((num_out, 3))
 
     return out
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+@cython.wraparound(False)   # Deactivate negative indexing.
+cdef _candidate_to_adj(np.int32_t[:, :] candidates, np.int8_t[:] success,
+                       np.int32_t[:] color_idx_map,
+                       int adj_N, int color_C,
+                       np.int32_t[:, :, :] out_adj):
+
+    cdef Py_ssize_t i, j, c, s, pos
+    for pos in range(candidates.shape[0]):
+        i = candidates[pos, 0]
+        j = candidates[pos, 1]
+        c = candidates[pos, 2]
+        if success[pos]:
+            out_adj[i, j, color_idx_map[c]] = 1
+            out_adj[j, i, color_idx_map[c]] = 1
+
+
+
+def candidate_to_adj(np.int32_t[:, :] candidates, np.int8_t[:] success,
+                     np.int32_t[:] color_idx_map,
+                     int adj_N, int color_C):
+    """
+    Convert a list of candidate edges and their successes to an adj map
+    """
+
+    out_adj = np.zeros((adj_N, adj_N, color_C), dtype=np.int32)
+    # cpdef int i, j, c, s 
+
+    # for (i, j, c), s in zip(candidates, success):
+    #     if s:
+    #         out_adj[i, j, color_idx_map[c]] = 1
+    #         out_adj[j, i, color_idx_map[c]] = 1
+    _candidate_to_adj(candidates, success, color_idx_map,
+                      adj_N, color_C, out_adj)
+    return out_adj
+
+def get_color_edge_possible_table(adj, node_colors, possible_edge_colors):
+    """
+    
+    
+    Returns NC x NC x EC matrix where 
+    p[nc_i, nc_j, ec] = number of edges of color ec in adj 
+    from node color i to node colorj 
+    """
+    possible_node_colors = np.max(node_colors)
+    out = np.zeros((possible_node_colors + 1,
+                    possible_node_colors +1,
+                    np.max(possible_edge_colors) + 1),
+                   dtype=np.int32)
+
+    for i in range(adj.shape[0]):
+        for j in range(i +1, adj.shape[1]):
+            nc_i = node_colors[i]
+            nc_j = node_colors[j]
+            ec = adj[i,j]
+            if ec > 0: # ec = 0 --> no edge
+                out[nc_i, nc_j, ec] += 1
+                if nc_i != nc_j: # don't double-count diagonal
+                    out[nc_j, nc_i, ec] += 1
+    return out
+    
+def filter_candidate_edges(sub_adj, sub_colors,
+                           main_adj, main_colors,
+                           possible_edge_colors, 
+                           candidate_edges):
+    """
+    Filter candidate edges using heuristics. 
+
+    1. If the main has no edges between color i and color j
+       of edge color c then we can skip testing 
+    ## FIXME add more
+
+    """
+
+    color_edge_possible_table = get_color_edge_possible_table(main_adj, main_colors,
+                                                          possible_edge_colors)
+    candidate_edges_out = []
+    for i, j, c in candidate_edges:
+        nc_i = sub_colors[i]
+        nc_j = sub_colors[j]
+        if color_edge_possible_table[i, j, c] > 0:
+            candidate_edges_out.append((i,j, c))
+    return np.array(candidate_edges_out, dtype=np.int32)
+
+            
+    
+
+    
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@ import numpy as np
 
 import pysubiso
 import time
-
+import itertools
+import networkx as nx
+import pytest
 
 def reference_gen_possible_next_edges(adj, colors):
     """
@@ -41,3 +43,135 @@ def test_possible_next_edges():
             print(f"{old_time/new_time:3.1f} times faster ({old_time*1e6:3.1f} us vs {new_time*1e6:3.1f} us)")
 
             np.testing.assert_array_equal(possible_edges, correct)
+
+
+def test_get_color_edge_possible_table():
+    """
+    Simple manual sanity-preserving examples
+    """
+
+    # (node colors) -edge colors-
+    
+    # (0) -1- (1) -2- (2) -3- (3)
+    
+    adj = np.array([[0, 1, 0, 0],
+                    [1, 0, 2, 0],
+                    [0, 2, 0, 3],
+                    [0, 0, 3, 0]], dtype=np.int32)
+    node_colors = np.array([0, 1, 2, 3], dtype=np.int32)
+    possible_edge_colors = np.array([1, 2, 3])
+    possible_node_colors = np.unique(node_colors)
+    
+    possible_table = pysubiso.get_color_edge_possible_table(adj, node_colors, possible_edge_colors)
+
+    ans = np.zeros((np.max(node_colors) + 1, np.max(node_colors)+1, np.max(possible_edge_colors)+1), dtype=np.int32)
+    ans[0, 1, 1] = 1
+    ans[1, 0, 1] = 1
+    ans[1, 2, 2] = 1
+    ans[2, 1, 2] = 1
+    ans[2, 3, 3] = 1
+    ans[3, 2, 3] = 1
+
+        
+    compare_node_edge_tables(possible_table, ans)
+
+    
+    # (0) -1- (1) -2- (1) -3- (0)
+    
+    adj = np.array([[0, 1, 0, 0],
+                    [1, 0, 2, 0],
+                    [0, 2, 0, 3],
+                    [0, 0, 3, 0]], dtype=np.int32)
+    node_colors = np.array([0, 1, 1, 0], dtype=np.int32)
+    possible_edge_colors = np.array([1, 2, 3])
+    possible_node_colors = np.unique(node_colors)
+    
+    possible_table = pysubiso.get_color_edge_possible_table(adj, node_colors, possible_edge_colors)
+
+    ans = np.zeros((np.max(node_colors) + 1, np.max(node_colors)+1, np.max(possible_edge_colors)+1), dtype=np.int32)
+    ans[0, 1, 1] = 1
+    ans[1, 0, 1] = 1
+    ans[1, 1, 2] = 1
+    ans[0, 1, 3] = 1
+    ans[1, 0, 3] = 1
+
+    
+    compare_node_edge_tables(possible_table, ans)
+    
+
+def sort_tuple(a):
+    return tuple(sorted(a))
+
+def compare_node_edge_tables(a, b):
+    assert a.shape == b.shape
+    for ec in range(a.shape[2]):
+        np.testing.assert_array_equal(a[:, :, ec], b[:, :, ec], err_msg=f"error, ec={ec}")
+
+@pytest.mark.parametrize('node_color_n', [1, 2, 3, 5, 8])
+@pytest.mark.parametrize('edge_color_n', [1, 2, 4, 8])
+@pytest.mark.parametrize('copies', [1, 2, 3, 4])
+def test_get_color_edge_possible_graph_gen(node_color_n, edge_color_n, copies):
+    """
+    Create graphs that will have a specific nodetype-edgetype stats by 
+    construction. 
+
+    """
+    np.random.seed(0)
+    for iters in range(10):
+        node_color_n = 4
+        edge_color_n = 6
+
+        possible_edge_colors = np.arange(edge_color_n) + 1
+        possible_node_colors = np.arange(node_color_n)
+
+        G = nx.Graph()
+        npos = 0
+        copies = 8
+        for i in range(copies):
+            for nc in possible_node_colors:
+                G.add_node(npos, color=nc)
+                npos += 1
+
+        nc_ec_stats = np.zeros((node_color_n, node_color_n, edge_color_n+1), dtype=np.int32)
+        for i in range(node_color_n):
+            for j in range(i+1, node_color_n):
+                for ec in range(1, edge_color_n + 1):
+                    nc_ec_stats[i, j, ec] = np.random.poisson(0.5)
+
+                    nc_ec_stats[j, i, ec] = nc_ec_stats[i, j, ec]
+        # random matrix
+
+        for i in range(node_color_n):
+            nc_i = possible_node_colors[i]
+            for j in range(i, node_color_n):
+                nc_j = possible_node_colors[j]
+                for ec in possible_edge_colors:
+                    ##
+                    tgt_number = nc_ec_stats[nc_i, nc_j, ec] # there should be this many edges between nodes of this color and nodes of that color
+                    for _ in range(tgt_number):
+                        nc_i_nodes = np.random.permutation([n for (n, d) in G.nodes(data=True) if d['color'] == nc_i])
+                        nc_j_nodes = np.random.permutation([n for (n, d) in G.nodes(data=True) if d['color'] == nc_j])
+                        unconnected = set([sort_tuple(p) for p in itertools.product(nc_i_nodes, nc_j_nodes)\
+                                       if p not in G.edges])
+                        if len(unconnected)  == 0:
+                            ## if there are no disconnected nodes of this color, add a new one
+
+                            new_node_id = len(G.nodes)
+                            G.add_node(new_node_id, color=nc_j)
+
+                            unconnected = set([(n, new_node_id) for p in nc_i_nodes])
+
+
+                        j= np.random.randint(0, len(unconnected))
+                        tgt_pair = list(unconnected)[j]
+                        print('adding edge between node colors', nc_i, nc_j, "with color=", ec) 
+                        G.add_edge(tgt_pair[0], tgt_pair[1], color=ec)
+
+
+
+
+        g_adj, g_color = pysubiso.util.nx_to_adj(G)
+        possible_table = pysubiso.get_color_edge_possible_table(g_adj, g_color, possible_edge_colors)
+        compare_node_edge_tables(possible_table, nc_ec_stats)
+
+
